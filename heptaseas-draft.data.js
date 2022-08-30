aeae216e38524e0bc5a4a79fc2d0d2d0a3d1ab4d
fo-rpl <toktokgaming1@gmail.com>
2022-08-30 12:53:18 +0700

    first commit

---
+++ heptaseas-draft.data.js
@@ -0,0 +1,56 @@
+
+ document.getElementById("name").value=`Penjelasan Item Blade Of The Heptases Mobile Legends`;
+ document.getElementById("texta").value=`<a style="color:black; font-size:1.1em; font-weight:450">Blade of the Heptases merupakan item physical yang lebih sering dipakai oleh hero Assassin. Item ini memiliki atribut 70 Physical attack, 250 HP sebagai atribut dasarnya dan 15 Physical pen sebagai atribut uniknya.
+</a>
+<br><br>
+<a style="color:black; font-size:1.1em; font-weight:450">
+Blade of the Heptases berfungsi untuk meningkatkan damage terhadap lawan dengan Physical Defense tinggi. jadi menjadi lebih berasa dan lebih menggigit.</a>
+<br>
+<br>
+<a style="color:black; font-size:1.1em; font-weight:450">
+Pasif unik item ini sangat menarik setiap kalian tidak memberi dan tidak menerima damage apapun untuk durasi lima detik tangan dari hero kalian akan bersinar yang menandai kalau pasif dari item ini sudah aktif.</a>
+<br>
+<H4>Berikut Atribut yang ada di item ini:</h4>
+<img src="read/vendor/gambar/996212714.png" width="100%"/>
+<br><br>
+<ul>
+<Li>
++70 physical attack
+</Li>
+<Li>
++250 HP
+</Li>
+<Li>
++15 physical penetration,
+Pasif unik - Ambush: Jika tidak ada damage yang diterima atau diberikan selama 5 detik, basic attack berikutnya akan memberikan damage yang setara dengan 160 dtambah 40% physical attack, dan menyebabkan efek slow sebesar 40% selama 1,5 detik.
+</Li>
+</ul>
+<br>
+<a style="color:black; font-size:1.1em; font-weight:450">
+Jika kamu belum mengerti mengenai efek mengenai item ini, secara singkatnya blade of heptaseas inia akan memberikan damage tambahan yang cukup menyakitkan pada basic attack pertama.
+<br><br>
+Item ini juga memiliki efek slow jika memukul lawan pada basic attack awal yang bisa membuatmu membunuh musuh dengan mudah.
+<br>
+<br>
+<h4>Berikut ini Hero yang cocok dengan item Blade of the heptaseas</h4>
+<br>
+<h5 style="color: darkslateblue; font-weight:600">Aldous</h5><p>
+Aldous ini sangat cocok dengan item ini, karena dengan skill 1 nya dan ditambah dengan pasif item ini akan membuat Basick attack dari Hero ini semakin menyakitkan, apalagi jika ditambah item Endless Batle.</p>
+<h5 style="color: darkslateblue; font-weight:600">Saber</h5>
+<p>
+Saber merupakan assassin paling mematikan bagi Hero yang darahnya tipis, dengan tambahan item ini akan membuat saber semakin ditakuti karena saber hanya butuh sekali ultimate + 1 Basick attack saja akan bisa dengan mudah mendapatkan kill.
+</p>
+<h5 style="color: darkslateblue; font-weight:600">Fanny</h5>
+<p>
+Fanny menjadi hero yang sangat cocok dengan item ini, karena hero ini juga butuh damage tambahan, damage tambahan ini juga akan membuat Fanny lebih cepat untuk farming.
+<br><br>
+Sedikit tips dari saya untuk Fanny dengan item ini pasti gunakan pasif nya dengan benar, saya sendiri biasanya menggunakan combo skill seperti ini 2+2+3+1+Basick Attack.
+</p>
+<h5 style="color: darkslateblue; font-weight:600">Chou</h5>
+<p>
+Dengan pasif nya, Chou akan menjadi sangat berdemage pada basic attack pertamanya, jadi sangat direkomendasikan untuk menggunakan item ini.
+</p>
+</a>`;
+ document.getElementById("metadeskripsi").value=`Ini Hero yang cocok dengan item Blade Of The Heptases`;
+ document.getElementById("metakeyword").value=``;
+ 
